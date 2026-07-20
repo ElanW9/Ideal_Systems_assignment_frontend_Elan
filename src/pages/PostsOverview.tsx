0eb87d1fb72168b9188e67bc161d250a0d5f0996
Elan Wittevrongel <elan.wittevrongel@gmail.com>
2026-07-20 20:19:58 +0200

    spinner 1
    add css for spinner (https://cssloaders.github.io/) + change return of loader

--- src/pages/PostsOverview.tsx
+++ src/pages/PostsOverview.tsx
@@ -7,17 +7,21 @@ import { getPosts } from '../api/posts';
 export default function PostsOverview() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     getPosts()
       .then(setPosts)
-      .catch((error) => console.error(error));
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
   }, []);
 
   const filteredPosts = posts.filter((post) =>
     post.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+  
+  if (loading) return <span className="loader"></span>;
   return (
     <section id="postsOverview" className="posts-overview-container">
       <PostSearchBar value={searchTerm} onChange={setSearchTerm} />
